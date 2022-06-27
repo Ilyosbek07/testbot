9d740d785789682bfcd0034896542dfa457b5b42
Ilyosbek <77951978+Ilyosbek07@users.noreply.github.com>
2022-06-27 14:14:19 +0500

    add drf

--- backend/panel/panel/urls.py
+++ backend/panel/panel/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+
+from backend.panel.tests.views import UserViewSet
+
 
 urlpatterns = [
     path('', admin.site.urls),
